f3fb3644b9bd549b967327d5ef27d85ca43db76e
Ukasz <xlukas1x@gmail.com>
2019-04-21 20:37:12 +0200

    Commit with small comments changes

--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -1,7 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 
 import {InMemoryDataService} from './in-memory-data.service';
-
+// No Tests included
 describe('InMemoryDataService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 });
